e8b6f41c242f747d5913630284031e42e139b84f
KLin <lin.kevin1@northeastern.edu>
2023-06-14 17:21:46 -0400

    Added Page 2 and 3.

--- app/src/main/java/edu/northeastern/mainactivity/MainActivity.java
+++ app/src/main/java/edu/northeastern/mainactivity/MainActivity.java
@@ -2,7 +2,9 @@ package edu.northeastern.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.lucky_button) {
+/*            Intent intent = new Intent(this, Page2.class);
+            startActivity(intent);*/
+        }
     }
 }
