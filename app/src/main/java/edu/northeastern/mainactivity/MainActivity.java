fc9556b3cd0ea6c348ee95c53c492445f84e2529
Kevin Lin <kevinlin5115@gmail.com>
2023-06-17 17:43:48 -0400

    Added adapter and recycler_view_main.xml layout for the Page 1 RecyclerView for daily article.

--- app/src/main/java/edu/northeastern/mainactivity/MainActivity.java
+++ app/src/main/java/edu/northeastern/mainactivity/MainActivity.java
@@ -1,22 +1,47 @@
 package edu.northeastern.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Handler stringHandler = new Handler();
     private static String dailyString;
+    private Button searchButton;
+    private Button luckyButton;
+    private EditText searchBar;
+    private RecyclerView dailyRV;
+    private DailyArticleAdapter dailyArticleAdapter;
+    private List<JsonObject> dailyList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchButton = findViewById(R.id.search_button);
+        luckyButton = findViewById(R.id.lucky_button);
+        searchBar = findViewById(R.id.search_bar);
+        dailyRV = findViewById(R.id.dailyRV);
+
+        dailyArticleAdapter = new DailyArticleAdapter(this, dailyList);
+        dailyRV.setLayoutManager(new LinearLayoutManager(this));
+        dailyRV.setAdapter(dailyArticleAdapter);
 
         APIThread runnableAPIThread = new APIThread();
         new Thread(runnableAPIThread).start();
@@ -24,12 +49,19 @@ public class MainActivity extends AppCompatActivity {
 
     class APIThread implements Runnable {
         // NOTE @shashankmanjunath: This APIThread class is just for testing
+        @SuppressLint("NotifyDataSetChanged")
         @Override
         public void run() {
             // JsonObject dailyArticle = APIMiddleware.getDailyArticle(getApplicationContext());
-            JsonArray jsonSearch = APIMiddleware.searchArticles("cantor set", 3, getApplicationContext());
+            //JsonArray jsonSearch = APIMiddleware.searchArticles("cantor set", 3, getApplicationContext());
             // JsonObject json = (JsonObject) JsonParser.parseString(dailyString);
-            Log.d("API Response", jsonSearch.get(0).toString());
+            //Log.d("API Response", jsonSearch.get(0).toString());
+
+            JsonObject daily = APIMiddleware.getDailyArticle(getApplicationContext());
+            runOnUiThread(() -> {
+                dailyList.add(daily);
+                dailyArticleAdapter.notifyDataSetChanged();
+            });
         }
     }
 }
