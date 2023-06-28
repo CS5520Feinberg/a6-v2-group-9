44a429d9756320ea21caade14bb14befe4e4ada3
Shashank Manjunath <shashankmanjunath14@gmail.com>
2023-06-28 14:16:12 -0400

    Created code for login and auth

--- app/src/main/java/edu/northeastern/mainactivity/LaunchActivity.java
+++ app/src/main/java/edu/northeastern/mainactivity/LaunchActivity.java
@@ -1,7 +1,5 @@
 package edu.northeastern.mainactivity;
 
-import static edu.northeastern.mainactivity.LoginActivity.dbTest;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,7 +14,7 @@ public class LaunchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
 
-        dbTest();
+        LoginActivity.registerUser("manjunath.sh@northeastern.edu");
 
         Button launchA6 = (Button) findViewById(R.id.launch_a6_button);
         launchA6.setOnClickListener(new View.OnClickListener() {
